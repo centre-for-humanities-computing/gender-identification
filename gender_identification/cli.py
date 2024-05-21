fb49c45cb39761529967867f4f2df7260f316c16
Márton Kardos <power.up1163@gmail.com>
2024-05-21 14:54:41 +0200

    Abstracted inference

--- gender_identification/cli.py
+++ gender_identification/cli.py
@@ -1,11 +1,10 @@
-import itertools
 from pathlib import Path
-from typing import Iterable, Union
+from typing import Union
 
 import pandas as pd
 from radicli import Arg, Radicli
-from tqdm import tqdm
-from transformers import pipeline
+
+from gender_identification.inference import add_gender
 
 cli = Radicli()
 
@@ -38,15 +37,6 @@ def write_table(table: pd.DataFrame, out_file: Path):
         )
 
 
-def batched(iterable: Iterable[str], n: int) -> Iterable[list[str]]:
-    # batched('ABCDEFG', 3) --> ABC DEF G
-    if n < 1:
-        raise ValueError("n must be at least one")
-    it = iter(iterable)
-    while batch := list(itertools.islice(it, n)):
-        yield batch
-
-
 @cli.command(
     "infer_gender",
     in_file=Arg(help="Input file path."),
@@ -66,6 +56,9 @@ def batched(iterable: Iterable[str], n: int) -> Iterable[list[str]]:
         "-d",
         help="Indicates whether to drop the column indicating the model's confidence in its predictions.",
     ),
+    batch_size=Arg(
+        "--batch_size", "-b", help="Size of the batches to do inference in."
+    ),
 )
 def infer_gender(
     in_file: Union[str, Path],
@@ -73,27 +66,21 @@ def infer_gender(
     out_file: Union[str, Path, None] = None,
     remove_last_name: bool = False,
     drop_confidence: bool = False,
+    batch_size: int = 32,
 ):
     in_file = Path(in_file)
     out_file = Path(out_file) if out_file is not None else in_file
     print("Loading data.")
     data = load_table(in_file)
-    names = data[name_column].tolist()
-    if remove_last_name:
-        names = [" ".join(name.split()[:-1]) for name in names]
-    print("Loading model.")
-    model = pipeline(model="Amanaccessassist/Gender-Classification")
-    results: list[dict] = []
-    progress = tqdm(names, desc="Inferring genders for all names.")
-    for batch in batched(progress, n=32):
-        results.extend(model(batch))  # type: ignore
-    results_df = pd.json_normalize(results).rename(
-        columns={"label": "gender", "score": "gender_confidence"}
+    print("Inferring results.")
+    data = add_gender(
+        data,
+        name_column=name_column,
+        remove_last_name=remove_last_name,
+        drop_confidence=drop_confidence,
+        batch_size=batch_size,
+        progress_bar=True,
     )
-    if drop_confidence:
-        results_df = results_df.drop(columns=["gender_confidence"])
-    results_df = results_df.set_index(data.index)
-    data = data.join(results_df)
     print("Saving results.")
     write_table(data, out_file)
     print("Done.")
